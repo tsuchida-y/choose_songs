bd113a388c7670edbdb2ad5a267b64ef899d8da5
tsuchida <yu41926t.tutida@icloud.com>
2025-01-02 02:12:30 +0900

    ページのレイアウト作成

--- lib/Song/song_page.dart
+++ lib/Song/song_page.dart
@@ -6,7 +6,6 @@ class SongPageles extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Firebase',
        theme: ThemeData(
          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
          useMaterial3: true,
@@ -30,7 +29,7 @@ class SongPageState extends State<SongPageful> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("曲一覧"),
         titleTextStyle: const TextStyle(
           color: Colors.black,
           fontSize: 35,
@@ -38,6 +37,51 @@ class SongPageState extends State<SongPageful> {
         ),
         backgroundColor: const Color.fromARGB(255, 255, 255, 240),
       ),
+      body:Center(
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+
+            return const Center(
+              child:Padding(
+                padding:EdgeInsets.symmetric(//上下左右に一定の余白を設定
+                  horizontal:16,//左右の余白
+                  vertical:10,//上下の余白
+                ),
+
+                child:Row(
+                  children:[
+                    Icon(
+                      Icons.music_note,
+                      size:30,
+                    ),
+
+                    Text(
+                      'テスト',
+                      style:TextStyle(
+                        fontSize:20,
+                        //fontWeight: FontWeight.bold,//テキストの太さ
+                      ),
+                    ),
+
+                  ]
+                )
+              )
+            );
+            
+          },
+        ),
+      ),
+
+
+        //曲を追加するボタン
+        floatingActionButton: FloatingActionButton(
+          
+          onPressed: () {
+          },
+          backgroundColor: const Color.fromARGB(255, 255, 255, 240),
+          child: const Icon(Icons.add),
+        ),
     );
   }
 }
